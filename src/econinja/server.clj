c83b528ed1e6ab8dd4de73ab806d8d2ddb0a19b6
Hermann Pauly <hp@campudus.com>
2018-05-13 13:59:55 +0200

    Remove aleph

--- src/econinja/server.clj
+++ src/econinja/server.clj
@@ -1,12 +1,12 @@
 (ns econinja.server
   (:gen-class)
-  (:require [aleph.http :as http]
+  (:require ; [aleph.http :as http]
             [econinja.handler :as en]
             [econinja.vars :refer [get-var]]
             [environ.core :refer [env]]))
 
-(defn -main [& _]
-  (let [port (Integer. (get-var :port))]
-    (println "Starting aleph on port" port)
-    (println "Serving from" (get-var :page-root))
-    (http/start-server en/app {:port port})))
+; (defn -main [& _]
+;   (let [port (Integer. (get-var :port))]
+;     (println "Starting aleph on port" port)
+;     (println "Serving from" (get-var :page-root))
+;     (http/start-server en/app {:port port})))
